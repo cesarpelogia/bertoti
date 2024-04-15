d3c22c6583c0690446e0bed2dd816a4b80f57a79
Cesar Pelogia <142257303+cesarpelogia@users.noreply.github.com>
2024-04-15 20:25:29 -0300

    Atualizando Bebida
    Transferindo atributos para Especificacao

--- Engenharia de Software/mercearia/src/mercearia/Bebida.java
+++ Engenharia de Software/mercearia/src/mercearia/Bebida.java
@@ -1,32 +1,24 @@
 package mercearia;
 
 public class Bebida {
+    
     // Atributos
-    private String tipoBebida;
-    private String marcaBebida;
-    private int quantidadeBebida;
+    private String nome;
+ 
 
-    // Metodos de Acesso Get
-    public String getTipoBebida() {
-        return tipoBebida;
+    public Bebida(String nome){
+        this.nome = nome;
     }
+       
+	public String getNome() {
+		return nome;
+	}
 
-    public String getMarcaBebida() {
-        return marcaBebida;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public int getQuantidadeBebida() {
-        return quantidadeBebida;
-    }
-
-    // Metodos de Acesso Set
-    public void setTipoBebida(String tipoBebida) {
-        this.tipoBebida = tipoBebida;
-    }
-
-    public void setMarcaBebida(String marcaBebida) {
-        this.marcaBebida = marcaBebida;
-    }
+}
 
     public void setQuantidadeBebida(int quantidadeBebida) {
         this.quantidadeBebida = quantidadeBebida;
