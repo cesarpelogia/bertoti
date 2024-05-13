666e6f6d0e8e62641209cf2afc7817a55a29b128
Cesar Pelogia <142257303+cesarpelogia@users.noreply.github.com>
2024-05-13 20:11:21 -0300

    Atualizando atividade

--- Engenharia de Software/mercearia/src/mercearia/Comida.java
+++ Engenharia de Software/mercearia/src/mercearia/Comida.java
@@ -1,23 +1,34 @@
 package mercearia;
 
 public class Comida {
+    
     // Atributos
-    private String nome;
+    private String nomeComida;
+    private String tipoComida;
+    private String marcaComida;
+    private int qntdComida;
 
-    public Comida(String nome){
-        this.nome = nome;
+    public Comida(String nomeComida, String tipoComida, String marcaComida, int qntdComida){
+        this.nomeComida = nomeComida;
+        this.tipoComida = tipoComida;
+        this.marcaComida = marcaComida;
+        this.qntdComida = qntdComida;
     }
        
-	public String getNome() {
-		return nome;
-	}
+    // Metodo Acesso nomeComida
+	public String getnomeComida() {return nomeComida;}
+	public void setnomeComida(String nomeComida) {this.nomeComida = nomeComida;}
 
-	public void setNome(String nome) {
-		this.nome = nome;
-    }
-}
+    // Metodo Acesso tipoComida Comida
+    public String gettipoComida() {return this.tipoComida;}
+    public void settipoComida(String tipoComida) {this.tipoComida = tipoComida;}
 
-    public void setQuantidadeComida(int quantidadeComida) {
-        this.quantidadeComida = quantidadeComida;
-    }
+    // Metodo Acesso marcaComida Comida
+    public String getmarcaComida() {return marcaComida;}
+    public void setmarcaComida(String marcaComida) {this.marcaComida = marcaComida;}
+
+    // Metodo Acesso Quantidade Comida
+    public int getQuantidade() {return qntdComida;}
+    public void setQuantidade(int qntdComida) {this.qntdComida = qntdComida;}
+
 }
