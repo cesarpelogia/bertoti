394dc7c1a651bd38af22cba65616f46d772a808b
Cesar Pelogia <142257303+cesarpelogia@users.noreply.github.com>
2024-05-13 20:12:10 -0300

    Atualizando atividade

--- Engenharia de Software/mercearia/src/mercearia/teste.java
+++ Engenharia de Software/mercearia/src/mercearia/teste.java
@@ -1,5 +1,14 @@
 package mercearia;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class teste {
-    
+
+
 }
