8901cf9a605a6b0bf1c96759e8b6557849c241b5
Cesar Pelogia <142257303+cesarpelogia@users.noreply.github.com>
2024-05-17 20:49:19 -0300

    Atualizando Teste

--- Engenharia de Software/mercearia/src/mercearia/teste.java
+++ Engenharia de Software/mercearia/src/mercearia/teste.java
@@ -1,14 +1,27 @@
 package mercearia;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.util.List;
-
-import org.junit.Before;
 import org.junit.Test;
 
 public class teste {
 
+    Mercearia cassio = new Mercearia();
+    
+    @Test
+    public void testeCadastrarComida(){
+        cassio.cadastrarComida("Macarrão", "Massa", "Renata", 3000);
+        cassio.cadastrarComida("Lasanha", "Massa", "Perdigão", 1000);
 
+        assertTrue(cassio.comidas.getLast().getnomeComida() == "Lasanha");
+    }
+
+    @Test
+    public void testeCadastrarBebida(){
+        cassio.cadastrarBebida("Vinho", "Merlot", "Danny", 3500);
+        cassio.cadastrarBebida("Cerveja", "Pilsen", "Brahma", 350);
+        
+        assertFalse(cassio.bebidas.getFirst().gettipoBebida() == "Pilsen");
+    }   
 }
