ba8c71677e14cb1312f160428c81c99c0149a7bb
Cesar Pelogia <142257303+cesarpelogia@users.noreply.github.com>
2024-05-17 20:46:27 -0300

    Atualizando Bebida

--- Engenharia de Software/mercearia/src/mercearia/Bebida.java
+++ Engenharia de Software/mercearia/src/mercearia/Bebida.java
@@ -1,33 +1,33 @@
-package mercearia;
+    package mercearia;
 
-public class Bebida {
+    public class Bebida {
+        
+        // Atributos
+        private String nomeBebida;
+        private String tipoBebida;
+        private String marcaBebida;
+        private int qntdBebida;
     
-    // Atributos
-    private String nomeBebida;
-    private String tipoBebida;
-    private String marcaBebida;
-    private int qntdBebida;
- 
-    public Bebida(String nomeBebida, String tipoBebida, String marcaBebida, int qntdBebida){
-        this.nomeBebida = nomeBebida;
-        this.tipoBebida = tipoBebida;
-        this.marcaBebida = marcaBebida;
-        this.qntdBebida = qntdBebida;    
+        public Bebida(String nomeBebida, String tipoBebida, String marcaBebida, int qntdBebida){
+            this.nomeBebida = nomeBebida;
+            this.tipoBebida = tipoBebida;
+            this.marcaBebida = marcaBebida;
+            this.qntdBebida = qntdBebida;    
+        }
+        
+        // Metodo Acesso nomeBebida
+        public String getnomeBebida() {return nomeBebida;}
+        public void setnomeBebida(String nomeBebida) {this.nomeBebida = nomeBebida;}
+
+        // Metodo Acesso tipoBebida Bebida
+        public String gettipoBebida() {return this.tipoBebida;}
+        public void settipoBebida(String tipoBebida) {this.tipoBebida = tipoBebida;}
+
+        // Metodo Acesso marcaBebida Bebida
+        public String getmarcaBebida() {return marcaBebida;}
+        public void setmarcaBebida(String marcaBebida) {this.marcaBebida = marcaBebida;}
+
+        // Metodo Acesso Quantidade Bebida
+        public int getQuantidade() {return qntdBebida;}
+        public void setQuantidade(int qntdBebida) {this.qntdBebida = qntdBebida;}
     }
-       
-    // Metodo Acesso nomeBebida
-	public String getnomeBebida() {return nomeBebida;}
-	public void setnomeBebida(String nomeBebida) {this.nomeBebida = nomeBebida;}
-
-    // Metodo Acesso tipoBebida Bebida
-    public String gettipoBebida() {return this.tipoBebida;}
-    public void settipoBebida(String tipoBebida) {this.tipoBebida = tipoBebida;}
-
-    // Metodo Acesso marcaBebida Bebida
-    public String getmarcaBebida() {return marcaBebida;}
-    public void setmarcaBebida(String marcaBebida) {this.marcaBebida = marcaBebida;}
-
-    // Metodo Acesso Quantidade Bebida
-    public int getQuantidade() {return qntdBebida;}
-    public void setQuantidade(int qntdBebida) {this.qntdBebida = qntdBebida;}
-}
